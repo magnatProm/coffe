7d0860ad8e365cb7d3aeeb606ca7f52171198f5f
magnatProm <109748213+magnatProm@users.noreply.github.com>
2023-05-23 11:27:50 +0300

    add

--- js.js
+++ js.js
@@ -16,105 +16,7 @@ function scroll(text) {
 }
 
   // ___________
-  let lengthCoffe = [];
-  window.addEventListener('click', (event) => {
 
-    const basketLike = event.target.closest('.card');
-   
-  
-    if (event.target.closest('.basket-like__item .btn_like')){
-      const like_form_data = event.target.closest(`.basket-like li`).getAttribute('data-like'),
-            like_form_li = document.querySelector(`.basket-like li[data-like="${like_form_data}"]`),
-            like_form_button = document.querySelector(`.card .card__info[data-n="${like_form_data}"] button`);
-
-      like_form_button.classList.remove('btn_like_active');
-      like_form_button.setAttribute('data-like', "");
-
-
-      like_form_li.parentNode.removeChild(like_form_li);
-    }
-
-  
-    const basketLikeContent = {
-      title: basketLike.querySelector('h3').innerText,
-      text: basketLike.querySelector('.card__inner p').innerText,
-      img: basketLike.querySelector('img').getAttribute('src'),
-      id: basketLike.querySelector('div').getAttribute('data-n'),
-    }
-
-  
-    if (event.target.dataset.like == "") {
-      event.target.setAttribute('data-like', "like");
-      event.target.closest('.btn_like').classList.add('btn_like_active');
-      lengthCoffe.push(basketLikeContent.id);
-  
-      document.querySelector('.basket-like').insertAdjacentHTML("beforeend",
-        `
-        <li class="dropdown__item list__btn basket-like__item" data-like="${basketLikeContent.id}">
-        <img src="${basketLikeContent.img}" alt="coffe" class="basket-like__img">
-        <div class="basket-like__text">
-        <h3>${basketLikeContent.title}</h3>
-        <p>${basketLikeContent.text}</p>
-        <p>${basketLikeContent.id}</p>
-        </div>
-        <button class="btn-circle btn_like btn_like_active" data-like="like">❤</button>
-        <div class="basket-like__inner" data-like="${basketLikeContent.id}">
-        </div>
-        </li>
-        `
-      );
-    }
-
-    // не нравиться
-    else {
-      const like_form = document.querySelector(`.basket-like li[data-like="${basketLikeContent.id}"]`);
-      event.target.setAttribute('data-like', "");
-      event.target.closest('button').classList.remove('btn_like_active');
-      like_form.style.background = "#3f51b5";
-      like_form.parentNode.removeChild(like_form);
-    }
-
-
-  
-  
-  }, { capture: true });
-  // ___________
-
-
-  // main button
-  window.addEventListener('click', function (event) {
-
-  const svg_number=event.target.closest('.svg').getAttribute('data-svg'),
-        svg__bread_top = document.querySelector(`.svg[data-svg="${svg_number}"] .svg__bread-top`),
-        svg__bread_bottom = document.querySelector(`.svg[data-svg="${svg_number}"] .svg__bread-bottom`),
-        svg__pouring = document.querySelector(`.svg[data-svg="${svg_number}"] .svg__pouring`);
-
-
-
-  if (svg__bread_top.hasAttribute('style')){
-        svg__bread_top.removeAttribute('style');
-        svg__bread_bottom.removeAttribute('style');
-        svg__pouring.removeAttribute('style');
-        return
-  }
-
-    if(event.target.closest('.svg') && !svg__bread_top.hasAttribute('style')){
-
-      function random(number) {
-        return Math.floor(Math.random() * (number+1));
-      }
-
-const fill_svg=`rgb(${random(255)} ${random(255)} ${random(255)})`;
-      
-      svg__bread_top.style.fill=fill_svg;
-      svg__bread_bottom.style.fill=fill_svg;
-      svg__pouring.style.fill=`rgb(${random(255)} ${random(255)} ${random(255)})`
-    } 
-
-  })
-  // main button
-
- 
 // slider bar
   sliderFunction(".slider__1")
   sliderFunction(".slider__2")
@@ -202,6 +104,129 @@ document.querySelector('.discount').onclick=function(){
 // button discount
 
 
-  onload = function () {  
-    document.querySelector('.loading').style.display = "none";
-  };
+
+
+
+// _________________
+window.addEventListener('click', (event) => {
+
+  if (event.target.closest('.product')) {
+      const basketLike = event.target.closest('.product');
+      const basketLikeContent = {
+        img: basketLike.querySelector('img').getAttribute('src'),
+        title: basketLike.querySelector('.product__title').innerText,
+        text: basketLike.querySelector('.product__text').innerText,
+        id: basketLike.getAttribute('data-n'),
+        btnLike: basketLike.querySelector('.product__inner .btn_like').className,
+        price: basketLike.querySelector('.product__inner p').getAttribute('data-price'),
+        discount: basketLike.getAttribute('data-discount'),
+        btn: basketLike.querySelector('.svg').innerHTML,
+      }
+      // btn: basketLike.querySelector('.svg').outerHTML,
+
+      // добавление товара
+      if (event.target.closest('.product[data-n] .btn_like') || event.target.closest('.main-btn')) {
+          add__choose(event.target, basketLikeContent)
+      }// добавление товара
+
+      // добавить стиль лайка
+      if (event.target.closest('.btn_like')) {
+        btn_like_style(basketLikeContent.id)
+      }// добавить стиль лайка
+
+      // добавить стиль
+      if (event.target.closest('.main-btn')) {
+        btn_style(basketLikeContent.id)
+      }// добавить стиль
+
+      basket_form_length()
+  }
+
+}, { capture: true });
+
+
+function add__choose(occasion, item) {
+  const form = (occasion.closest('.main-btn')) ? '.basket-product' : '.basket-like';
+  const text_2 = (occasion.closest('.main-btn')) ? '' : `data-like="${item.id}"`;
+
+  const deleteBasket = document.querySelectorAll(`.basket-like [data-like="${item.id}"]`).length;
+  const deleteBasketP = document.querySelectorAll(`.basket-product [data-n="${item.id}"]`).length;
+  
+  const discount = (item.discount=='null') ? '' :  `<p class="btn-circle discount__btn">${item.discount}%</p>`;
+
+  if (occasion.closest('.btn_like') && deleteBasket > 0) {
+      document.querySelector(`.basket-like [data-like="${item.id}"]`).remove()
+  }
+  else if(occasion.closest('.main-btn') && deleteBasketP > 0) {
+    document.querySelector(`.basket-product [data-n="${item.id}"]`).remove()
+  }
+  else {
+      document.querySelector(`${form}`).insertAdjacentHTML("afterbegin",
+      `
+      <li class="product" ${text_2} data-n="${item.id}" data-discount="${item.discount}">
+        <img class="product__img" src="${item.img}" alt="coffe">
+        ${discount}
+        <div class="product__info">
+        <h3 class="product__title">${item.title}</h3>
+        <p class="product__text list__text">${item.text}</p>
+        <div class="product__inner">
+        <p class="product__price" data-price="${item.price}">${item.price}$</p>
+        <button class="${item.btnLike}">❤</button>
+
+        <button class="main-btn">
+        <svg class="svg" width="60" height="40" viewBox="56 0 400 330" fill="none">
+        ${item.btn}
+        </svg>
+        </button>
+        
+        </div>
+        </div>
+        </li>
+      `
+      );
+  }
+}
+
+function btn_like_style(like){
+  const text=document.querySelectorAll(`[data-n="${like}"] .btn_like`);
+  text.forEach(item=> item.classList.toggle('btn_like_active'))
+}
+
+function btn_style(btn){
+  const text=document.querySelectorAll(`[data-n="${btn}"] .svg`);
+  const svg__bread_top = document.querySelector(`[data-n="${btn}"] .svg__bread-top`);
+  const fill_svg=`rgb(${random(255)} ${random(255)} ${random(255)})`;
+  const fill_svg_2=`rgb(${random(255)} ${random(255)} ${random(255)})`;
+
+  function random(number) {
+    return Math.floor(Math.random() * (number+1));
+  }
+  
+  if(!svg__bread_top.hasAttribute('style')){    
+      text.forEach(item=> {
+        item.querySelector('.svg__bread-top').style.fill=fill_svg
+        item.querySelector('.svg__bread-bottom').style.fill=fill_svg
+        item.querySelector('.svg__pouring').style.fill=fill_svg_2
+        console.dir(item)
+      })
+    } else{
+      text.forEach(item=> {
+        item.querySelector('.svg__bread-top').removeAttribute('style')
+        item.querySelector('.svg__bread-bottom').removeAttribute('style')
+        item.querySelector('.svg__pouring').removeAttribute('style')
+      })
+    }
+}
+
+function basket_form_length(name='product', number=0){
+  const text=document.querySelectorAll(`.basket-${name} li`).length;
+  const info=((text==0) ? '' : text)
+
+  document.querySelectorAll('.list .list__item .label')[number].innerHTML=info;
+
+  if(number==0){
+    basket_form_length(name='like', number=1)
+  } else{
+    return
+  }
+}
